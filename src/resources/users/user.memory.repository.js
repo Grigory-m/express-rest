508343de53822b4ae8eab2be0c93159fcb5f902e
Grigory Murashka <gm201212@gmail.com>
2021-05-26 16:39:15 +0300

    feat: add jsdoc comments into users's folder

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,19 +1,43 @@
 const users = [];
 
+/**
+ * Returns all users
+ * @returns {array} Array of users
+ */
 const getAll = async () => users;
 
+/**
+ * Returns user by id
+ * @param {string} id 
+ * @returns {object} a required user
+ */
 const getById = async (id) => users.find(user => user.id === id);
 
+/**
+ * Creates new user
+ * @param {object} user 
+ * @returns void
+ */
 const create = async (user) => {  
   users.push(user);
 };
 
+/**
+ * Returns updated user
+ * @param {object} user 
+ * @returns {object} an updated user
+ */
 const update = async (user) => {
   const index = users.findIndex(item => user.id === item.id);
   users[index] = user;
   return user;
 };
 
+/**
+ * Deletes user by id
+ * @param {string} id 
+ * @returns void
+ */
 const remove = async (id) => {
   const index = users.findIndex(item => id === item.id);
   users.splice(index, 1);
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,10 +1,38 @@
 const usersRepo = require('./user.memory.repository');
 const tasksService = require('../tasks/task.memory.repository');
 
+/**
+ * Returns all users
+ * @returns {array} Array of users
+ */
 const getAll = () => usersRepo.getAll();
+
+/**
+ * Returns user by id
+ * @param {string} id 
+ * @returns {object} a required user
+ */
 const getById = (id) => usersRepo.getById(id);
+
+/**
+ * Creates new user
+ * @param {object} user 
+ * @returns void
+ */
 const create = (user) => usersRepo.create(user);
+
+/**
+ * Returns updated user
+ * @param {object} user 
+ * @returns {object} an updated user
+ */
 const update = (user) => usersRepo.update(user);
+
+/**
+ * Deletes user by id
+ * @param {string} id 
+ * @returns void
+ */
 const remove = async (id) => {
   usersRepo.remove(id);
   tasksService.tasks.forEach(item => {
