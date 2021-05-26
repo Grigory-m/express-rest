c00506cd448d66ba4c02861a31c786062eedcea9
Grigory Murashka <gm201212@gmail.com>
2021-05-26 12:09:59 +0300

    feat: add jsdoc comments into board's folder

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,21 +1,41 @@
 const boards = [];
 
+/**
+ * Returns all boards
+ * @returns {array} Array of boards
+ */
 const getAll = async () => boards;
 
-const getById = async (id) => {
-  return boards.find(board => board.id === id);;
-};
+/**
+ * Returns board by id
+ * @param {string} id  
+ * @returns {object} a required board
+ */
+const getById = async (id) => boards.find(board => board.id === id);
 
-const create = async (board) => {  
-  boards.push(board);
-};
+/**
+ * Creates new board
+ * @param {object} board 
+ * @returns void
+ */
+const create = async (board) => boards.push(board);
 
+/**
+ * Returns updated board
+ * @param {object} board 
+ * @returns {object} an updated board
+ */
 const update = async (board) => {
   const index = boards.findIndex(item => board.id === item.id);
   boards[index] = board;
   return board;
 };
 
+/**
+ * Deletes board by id
+ * @param {string} id 
+ * @returns void
+ */
 const remove = async (id) => {
   const index = boards.findIndex(item => id === item.id);
   boards.splice(index, 1);
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,15 +1,43 @@
 const boardsRepo = require('./board.memory.repository');
 const tasksService = require('../tasks/task.service');
 
+/**
+ * Returns all boards
+ * @returns {array} Array of boards
+ */
 const getAll = () => boardsRepo.getAll();
+
+/**
+ * Returns board by id
+ * @param {string} id 
+ * @returns {object} a required board
+ */
 const getById = (id) => boardsRepo.getById(id);
+
+/**
+ * Creates new board
+ * @param {object} board 
+ * @returns void
+ */
 const create = (board) => boardsRepo.create(board);
+
+/**
+ * Returns updated board
+ * @param {object} board 
+ * @returns {object} an updated board
+ */
 const update = (board) => boardsRepo.update(board);
+
+/**
+ * Deletes board by id
+ * @param {string} id 
+ * @returns void
+ */
 const remove = async (id) => {
   const tasks = await tasksService.getAll(id);
   const removedTasks = tasks.map((task) => tasksService.remove(task.id));
   await Promise.all(removedTasks);
-  return boardsRepo.remove(id);
+  boardsRepo.remove(id);
 };
 
 module.exports = { getAll, getById, create, update, remove };
